012faefa82c40e066665f7d405b9d3a3041ad4b3
Victor Zverovich <viz@meta.com>
2025-07-13 09:05:29 -0700

    Improve test naming consistency

--- src/dragonbox_test.cpp
+++ src/dragonbox_test.cpp
@@ -1,13 +1,13 @@
 #include "dragonbox/dragonbox_to_chars.h"
 #include "test.h"
 
-void dtoa_dragonbox(double x, char* buffer) {
-  jkj::dragonbox::to_chars(x, buffer, jkj::dragonbox::policy::cache::full);
-}
-
 void dtoa_dragonbox_comp(double x, char* buffer) {
   jkj::dragonbox::to_chars(x, buffer, jkj::dragonbox::policy::cache::compact);
 }
 
-REGISTER_TEST(dragonbox);
+void dtoa_dragonbox_full(double x, char* buffer) {
+  jkj::dragonbox::to_chars(x, buffer, jkj::dragonbox::policy::cache::full);
+}
+
 REGISTER_TEST(dragonbox_comp);
+REGISTER_TEST(dragonbox_full);

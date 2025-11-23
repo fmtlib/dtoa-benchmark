cea1d9ccf59cf3e6206d640106338c7a047aa459
Victor Zverovich <viz@meta.com>
2025-11-23 09:16:12 -0800

    Skip compact tables for Dragonbox

--- src/dragonbox_test.cpp
+++ src/dragonbox_test.cpp
@@ -1,13 +1,8 @@
 #include "dragonbox/dragonbox_to_chars.h"
 #include "test.h"
 
-void dtoa_dragonbox_comp(double x, char* buffer) {
-  jkj::dragonbox::to_chars(x, buffer, jkj::dragonbox::policy::cache::compact);
-}
-
-void dtoa_dragonbox_full(double x, char* buffer) {
+void dtoa_dragonbox(double x, char* buffer) {
   jkj::dragonbox::to_chars(x, buffer, jkj::dragonbox::policy::cache::full);
 }
 
-REGISTER_TEST(dragonbox_comp);
-REGISTER_TEST(dragonbox_full);
+REGISTER_TEST(dragonbox);
--- src/fmttest.cpp
+++ src/fmttest.cpp
@@ -4,9 +4,9 @@
 #include "test.h"
 #include "fmt/compile.h"
 
-void dtoa_fmt_comp(double value, char* buffer) {
+void dtoa_fmt(double value, char* buffer) {
   buffer = fmt::format_to(buffer, FMT_COMPILE("{}"), value);
   *buffer = '\0';
 }
 
-REGISTER_TEST(fmt_comp);
+REGISTER_TEST(fmt);

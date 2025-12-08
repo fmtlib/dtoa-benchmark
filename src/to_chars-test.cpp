73b5a037116af1aa67da93c1b7b8c59b2aa50b7f
Victor Zverovich <viz@meta.com>
2025-12-08 15:55:37 -0800

    cleanup

--- src/to_chars-test.cpp
+++ src/to_chars-test.cpp
@@ -8,9 +8,3 @@ void dtoa_to_chars(double value, char* buffer) {
 }
 
 REGISTER_TEST(to_chars);
-
-void dtoa_format(double value, char* buffer) {
-  *std::format_to(buffer, "{}", value) = '\0';
-}
-
-REGISTER_TEST(format);

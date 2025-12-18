b4fac855856da6743fe5078396870af6e51b5ff0
Victor Zverovich <viz@meta.com>
2025-12-17 17:59:51 -0800

    Filter out ridiculously slow methods

--- result/template.php
+++ result/template.php
@@ -171,9 +171,26 @@ function drawTable(type, timeData) {
 }
 
 function drawBarChart(type, timeData) {
-    var defaultColors = ["#3366cc","#dc3912","#ff9900","#109618","#990099","#0099c6","#dd4477","#66aa00","#b82e2e","#316395","#994499","#22aa99","#aaaa11","#6633cc","#e67300","#8b0707","#651067","#329262","#5574a6","#3b3eac","#b77322","#16d620","#b91383","#f4359e","#9c5935","#a9c413","#2a778d","#668d1c","#bea413","#0c5922","#743411"];
+  var defaultColors = [
+    "#3366cc","#dc3912","#ff9900","#109618","#990099","#0099c6","#dd4477",
+    "#66aa00","#b82e2e","#316395","#994499","#22aa99","#aaaa11","#6633cc",
+    "#e67300","#8b0707","#651067","#329262","#5574a6","#3b3eac","#b77322",
+    "#16d620","#b91383","#f4359e","#9c5935","#a9c413","#2a778d","#668d1c",
+    "#bea413","#0c5922","#743411"
+  ];
 
-	var data = google.visualization.arrayToDataTable(timeData);
+  // Filter out methods that are too slow.
+  var excluded = {
+    "ostringstream": true,
+    "sprintf": true
+  };
+  var filtered = [timeData[0]];
+  for (var i = 1; i < timeData.length; i++) {
+    if (!excluded[timeData[i][0]])
+      filtered.push(timeData[i]);
+  }
+
+	var data = google.visualization.arrayToDataTable(filtered);
 	data.addColumn({ type: "string", role: "style" })
 	for (var rowIndex = 0; rowIndex < data.getNumberOfRows(); rowIndex++)
 		data.setValue(rowIndex, 2, defaultColors[rowIndex]);
